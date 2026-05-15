08830f27c79f884e466a031b127897ca3c22d822
Bad_Mitches <mitchell.gegich@gmail.com>
2026-05-15 14:10:03 -0400

    Update clan tag validator to be more rigorous, remove unnecessary cards folder from validators

--- src/middleware/validators/clans/validateClanTag.ts
+++ src/middleware/validators/clans/validateClanTag.ts
@@ -13,5 +13,25 @@ export function validateClanTag(
     });
   }
 
+  if (!clanTag.startsWith("#")) {
+    return res.status(400).json({
+      message: "Invalid clan tag: missing '#' at beginning of tag",
+    });
+  }
+
+  if (/[a-z]/.test(clanTag)) {
+    return res.status(400).json({
+      message: "Invalid clan tag: lowercase characters are not allowed",
+    });
+  }
+
+  const validClanTagRegex = /^#[0289PYLQGRJCUV]+$/;
+
+  if (!validClanTagRegex.test(clanTag)) {
+    return res.status(400).json({
+      message: "Invalid clan tag: format is unacceptable",
+    });
+  }
+
   next();
 }
